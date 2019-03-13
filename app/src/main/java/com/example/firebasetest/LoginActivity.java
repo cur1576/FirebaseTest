9c3085cf0c931bbd477fe362c6f5a6e2301f6574
cur1576 <cur1576@gmx.de>
2019-03-13 13:21:50 +0100

    commit_13.03._06

--- app/src/main/java/com/example/firebasetest/LoginActivity.java
+++ app/src/main/java/com/example/firebasetest/LoginActivity.java
@@ -2,18 +2,33 @@ package com.example.firebasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth auth;
+    private TextView statusTextView, detailTextView;
+    private EditText emailField, passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        statusTextView = findViewById(R.id.status);
+        detailTextView = findViewById(R.id.detail);
+        emailField = findViewById(R.id.fieldEmail);
+        passwordField = findViewById(R.id.fieldPassword);
+
+        findViewById(R.id.emailSignInButton).setOnClickListener(this);
+        findViewById(R.id.emailCreateAccountButton).setOnClickListener(this);
+        findViewById(R.id.signOutButton).setOnClickListener(this);
+        findViewById(R.id.verifyEmailButton).setOnClickListener(this);
 
         auth = FirebaseAuth.getInstance();
     }
@@ -26,6 +41,41 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void updateUI(FirebaseUser currentUser) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (detailTextView.getId()) {
+            case R.id.emailCreateAccountButton:
+                creatAccount(emailField.getText().toString(), passwordField.getText().toString());
+                break;
+            case R.id.emailSignInButton:
+                signIn(emailField.getText().toString(), passwordField.getText().toString());
+                break;
+            case R.id.signOutButton:
+                signOut();
+                break;
+            case R.id.verifyEmailButton:
+                sendEmailVerification();
+                break;
+        }
+
+    }
+
+    private void creatAccount(String email, String pwd) {
+
+    }
+
+    private void signIn(String email, String pwd) {
+
+    }
+
+    private void signOut() {
+
+    }
+
+    private void sendEmailVerification() {
 
     }
 }

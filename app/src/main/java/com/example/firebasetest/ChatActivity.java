5c7faf5a78b74a3046bfc6acec8794664946b4ff
cur1576 <cur1576@gmx.de>
2019-03-15 09:21:07 +0100

    commit_15.03._02

--- app/src/main/java/com/example/firebasetest/ChatActivity.java
+++ app/src/main/java/com/example/firebasetest/ChatActivity.java
@@ -1,97 +1,72 @@
 package com.example.firebasetest;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class ChatActivity extends AppCompatActivity {
 
-    EditText input, et_user;
-    TextView output;
+    EditText messageArea;
+    ImageView sendButton;
+    LinearLayout layout;
+    RelativeLayout layout_2;
+    ScrollView scrollView;
+
+    String username,chatWith;
 
     FirebaseAuth auth;
     FirebaseUser user;
     FirebaseDatabase database;
-    DatabaseReference reference;
-    DatabaseReference childReference;
+    DatabaseReference reference1, reference2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        input = findViewById(R.id.input);
-        et_user = findViewById(R.id.user);
-        output = findViewById(R.id.output);
-        output.setText("");
-        Message fromServer = new Message();
+        layout = findViewById(R.id.layout1);
+        layout_2 = findViewById(R.id.layout2);
+        scrollView = findViewById(R.id.scrollView);
+        messageArea = findViewById(R.id.messageArea);
+        sendButton = findViewById(R.id.sendButton);
+
+        username = auth.getCurrentUser().getEmail();
+        chatWith = getIntent().getStringExtra("chatWith");
 
         database = FirebaseDatabase.getInstance();
-        auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
-        et_user.setText(user.getEmail());
-        reference = database.getReference("messages");
-//        childReference = reference.child("message");
 
+        reference1 = database.getReference("messages/" + username + "_" + chatWith);
+        reference2 = database.getReference("messages/" + chatWith + "_" + username);
 
-//        reference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                Message value = dataSnapshot.getValue(Message.class);
-//                if(value!=null){
-//                    output.setText(value.getUser() + ": " + value.getText());
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                Toast.makeText(ChatActivity.this, "Fehler beim lesen der Daten", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-        reference.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Message value = dataSnapshot.getValue(Message.class);
-                if(value!=null){
-                    output.append(value.getUser() + ": " + value.getText() + "\n");
-                }
+        sendButton.setOnClickListener((v)->{
+            String messageText = messageArea.getText().toString();
+            if(!TextUtils.isEmpty(messageText)){
+                Map<String, String>map = new HashMap<>();
+                map.put("message",messageText);
+                map.put("user",username);
+                reference1.push().setValue(map);
+                reference2.push().setValue(map);
+                messageArea.setText("");
             }
 
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
         });
+
     }
 
     @Override
@@ -110,9 +85,5 @@ public class ChatActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void send(View view) {
-        Message message = new Message(et_user.getText().toString(),input.getText().toString());
-        childReference = reference.push();
-        childReference.setValue(message);
-    }
+
 }
